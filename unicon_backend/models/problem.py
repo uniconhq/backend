3175e44db718596a578d642cded6dec0415eac67
seeleng <seelengxd@gmail.com>
2025-03-09 12:50:52 +0800

    feat: add max_attempt to task view/edit apis

--- unicon_backend/models/problem.py
+++ unicon_backend/models/problem.py
@@ -140,6 +140,7 @@ class TaskORM(CustomSQLModel, table=True):
             description: str | None,
             autograde: bool,
             order_index: int,
+            max_attempts: int | None,
             **other_fields,
         ):
             return TaskORM(
@@ -149,6 +150,7 @@ class TaskORM(CustomSQLModel, table=True):
                 description=description,
                 autograde=autograde,
                 order_index=order_index,
+                max_attempts=max_attempts,
                 other_fields=other_fields,
             )
 
@@ -164,6 +166,7 @@ class TaskORM(CustomSQLModel, table=True):
                 "autograde": self.autograde,
                 "order_index": self.order_index,
                 "problem_id": self.problem_id,
+                "max_attempts": self.max_attempts,
                 "updated_version_id": self.updated_version_id,
                 **self.other_fields,
             }
